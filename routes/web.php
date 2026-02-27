8f816371cc723b4e000b2a6b37edde7f8cffc687
SzymCode <szymon.radomski@yahoo.com>
2026-02-27 15:35:24 +0100

    feat: relocate logout route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Auth::routes(['logout' => false]);
+
+Route::post('/logout', [LogoutController::class, 'logout'])
+    ->middleware(['web', 'auth'])
+    ->name('logout');
